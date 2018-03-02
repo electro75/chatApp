74d7e5bf5c7385e9253f822bc84e6794055da591
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-03-02 23:22:52 +0530

    adds autoscrolling feature

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,22 @@ socket.on('connect', function(){
 
 })
 
+function scrollToBottom() {
+    //Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight ) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('welcome', function(msg){
     console.log(msg.text)
 })
@@ -29,7 +45,7 @@ socket.on('newMessage', function(msg){
     });
 
     $('#messages').append(html);
-
+    scrollToBottom();
     
     // var li = $('<li></li>');
     // li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
@@ -87,7 +103,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     $('#messages').append(html);
-
+    scrollToBottom();
     // var li = $('<li></li>');
     // var a = $('<a target="_blank"  >My Lokesh</a>');
 
